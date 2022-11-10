45d1a116c319f28074d68ebc6d4949dec75de5a2
pabloGm1204 <pabloGm1204@gmail.com>
2022-11-10 08:44:02 +0100

    Ejercicio12

--- Ejercicio12.java
+++ Ejercicio12.java
@@ -2,9 +2,10 @@ public class Ejercicio12{
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Este programa llena la pantalla de caracteres aleatorios del codigo ascii");
         System.out.print("\033[32m");
-        for(int i = 1; i<100000; i++){
+        for(int i = 1; i<10000; i++){
             System.out.print((char)(Math.random()*(126-32+1)+32));
             Thread.sleep(1);
         }
+        System.out.print("\033[0m");
     }
 }
