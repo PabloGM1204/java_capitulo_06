cbe1c9636122671fc13dcbdec16cb08f534e70ca
pabloGm1204 <pabloGm1204@gmail.com>
2022-11-10 08:44:13 +0100

    Ejercicio14

--- Ejercicio14.java
+++ Ejercicio14.java
@@ -3,31 +3,25 @@ public class Ejercicio14{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Este programa intetara adivinar tu número");
-        System.out.print("Dime el número a adivinar: ");
-        int n = sc.nextInt();
         int max = 100;
         int min = 0;
         int na = 0;
-        boolean acertado = true;
+        boolean acertado = false;
         for(int i = 1; i<=5; i++){
-            int a = (int)(Math.random()*(max-min)+min);
-            na = a;
-            System.out.print(a+" ");
-            if(a == n){
-                i = 6;
-                System.out.println("He adivinado el número");
+            int numpen = (int)(Math.random()*(max-min)+min);
+            System.out.print("¿Es el "+numpen+" ?");
+            System.out.print("El número que estás pensando es 1) mayor 2) menor 3) el mismo: ");
+            int mayMenIgua = sc.nextInt();
+            if(mayMenIgua == 1){
+                min = numpen+1;
+            }else if(mayMenIgua == 2){
+                max = numpen-1;
+            }else if(mayMenIgua == 3){
                 acertado = true;
+                System.out.println("Has acertado");
+                i = 6;
             }else{
-                acertado = false;
-                System.out.print("Tu número es mayor o menor: ");
-                String ayuda = sc.next();
-                if(ayuda.equals("mayor")){
-                    min = na-1;
-                }else if(ayuda.equals("menor")){
-                    max = na+1;
-                }else{
-                    System.out.println("Error, debes escribir bien");
-                }
+                System.out.println("Error, debes escribir bien");
             }
         }if(!acertado){
             System.out.println("Me he quedado sin intentos");
