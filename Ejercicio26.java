fbdf76bdbdd0d4429a424a1b86748f300ba2f47a
pabloGm1204 <pabloGm1204@gmail.com>
2022-11-15 09:32:40 +0100

    Ejercicio26

--- Ejercicio26.java
+++ Ejercicio26.java
@@ -7,16 +7,18 @@ public class Ejercicio26{
         int anc = sc.nextInt();
         System.out.print("Introduzca la altura de la tabla: ");
         int alt = sc.nextInt();
-        int n = (int)(Math.random()*alt)+1;
+        int n = (int)(Math.random()*(anc*2+(alt-2)*2));
+        int pos = 0;
         for(int i = 1; i<=alt; i++){
-            int j = 1;
-            if(i == n){
-                for(j = 2; j<=anc; j++){
+            for(int j = 1; j<=anc; j++){
+                Boolean borde = (i == 1) || (i == alt) || (j == 1) || (j == anc);
+                if((pos == n) && borde){
+                    System.out.print(" ");
+                }else{
                     System.out.print("*");
                 }
-            }else{
-                for(j = 1; j<=anc; j++){
-                    System.out.print("*");
+                if(borde){
+                    pos++;
                 }
             }
             System.out.println("");
