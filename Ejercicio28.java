238954eebd38177cfe9afd7a9d65f48ba486f541
pabloGm1204 <pabloGm1204@gmail.com>
2022-11-15 12:58:55 +0100

    Ejercicio28

---
+++ Ejercicio28.java
@@ -0,0 +1,5 @@
+public class Ejercicio28{
+    public static void main(String[] args) {
+        System.out.println("Este programa que sea capaz de recolocar los números de un array de fuera hacia adentro");
+    }
+}
