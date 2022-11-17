8522b4a9bf78ee9a969a06c4798de4726dafba3e
pabloGm1204 <pabloGm1204@gmail.com>
2022-11-17 10:30:12 +0100

    Ejercicio31

---
+++ Ejercicio31.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Ejercicio31{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Este programa realiza el juego de craps");
+        System.out.print("Cuanto dinero quieres apostar: ");
+        int apuesta = sc.nextInt();
+        boolean acertado = false;
+        int d1 = (int)(Math.random()*6)+1;
+        int d2 = (int)(Math.random()*6)+1;
+        int resultado
+        System.out.println("1ºDado: "+d1+" 2ºDado: "+d2);
+        if((d1+d2)== 7 || (d1+d2)== 11){
+            System.out.println("Ha salido: "+(d1+d2));
+            System.out.println("¡¡Has ganado!!Tu dinero total ahora es: "+(apuesta*2));
+            acertado = true;
+        }else if((d1+d2) == 2 || (d1+d2) == 3 || (d1+d2)==12){
+            System.out.println("Ha salido: "+(d1+d2));
+            System.out.println("Has perdido por lo que no te queda dinero");
+        }else{
+            System.out.println("Ha salido: "+(d1+d2)+" por lo que hay que tirar otra vez");
+            d1 = (int)(Math.random()*6)+1;
+            d2 = (int)(Math.random()*6)+1;
+            System.out.println("Ha salido: "+(d1+d2));
+            if((d1)){
+
+            }
+        }
+    }
+}
