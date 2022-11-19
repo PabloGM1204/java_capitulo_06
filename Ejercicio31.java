2597b3714f5989af8f55d3ad0bd370c9d6adb1c4
pabloGm1204 <pabloGm1204@gmail.com>
2022-11-19 15:06:55 +0100

    Ejercicio31

--- Ejercicio31.java
+++ Ejercicio31.java
@@ -8,7 +8,7 @@ public class Ejercicio31{
         boolean acertado = false;
         int d1 = (int)(Math.random()*6)+1;
         int d2 = (int)(Math.random()*6)+1;
-        int resultado
+        int resultado = d1+d2;
         System.out.println("1ºDado: "+d1+" 2ºDado: "+d2);
         if((d1+d2)== 7 || (d1+d2)== 11){
             System.out.println("Ha salido: "+(d1+d2));
@@ -16,15 +16,23 @@ public class Ejercicio31{
             acertado = true;
         }else if((d1+d2) == 2 || (d1+d2) == 3 || (d1+d2)==12){
             System.out.println("Ha salido: "+(d1+d2));
-            System.out.println("Has perdido por lo que no te queda dinero");
+            System.out.println("Has perdido");
         }else{
             System.out.println("Ha salido: "+(d1+d2)+" por lo que hay que tirar otra vez");
-            d1 = (int)(Math.random()*6)+1;
-            d2 = (int)(Math.random()*6)+1;
-            System.out.println("Ha salido: "+(d1+d2));
-            if((d1)){
-
-            }
+            do{
+                d1 = (int)(Math.random()*6)+1;
+                d2 = (int)(Math.random()*6)+1;
+                if(resultado == d1+d2){
+                    System.out.println("Tu número es "+(d1+d2)+" por lo tanto has ganado");
+                    acertado = true;
+                }else if(resultado == 7){
+                    System.out.println("Tu número es "+(d1+d2)+" por lo que has perdido");
+                    acertado = true;
+                }else{
+                    System.out.println("Tu número es "+(d1+d2)+" por lo que debes seguir jugando");
+                    resultado = d1+d2;
+                }
+            }while(!acertado);
         }
     }
 }
